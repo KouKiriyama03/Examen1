6e954426d2eff7f7de98c34bc7f3b00a3ed20822
KouKiriyama03 <josuepanky@gmail.com>
2020-10-15 17:59:42 -0600

    Creación del main.hbs y la conceción al index

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 // import express
 const express = require("express");
 
+// Importar handlebars como template engine
+const exphbs = require("express-handlebars");
+
 // Crear un servidor express
 const app = express();
+
+// Indicar a express utilizar handlebars como template engine
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+
 
 // Inicialización del servidor en un puerto especifico
 app.listen(5005, () => {
