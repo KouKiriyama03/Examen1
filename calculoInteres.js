635f61323fd7556b8dcb153527ce1779faac3b56
KouKiriyama03 <josuepanky@gmail.com>
2020-10-15 21:31:08 -0600

    Creacción de for para metodo para el calculo de impuesto

--- calculoInteres.js
+++ calculoInteres.js
@@ -5,25 +5,23 @@
 // n es el período de tiempo (en meses).
 
 
-exports.calcularInteresCompuesto = (capital, tasaInteres, tiempo) => {
+exports.calcularInteresCompuesto = (monto, tasaInteres, tiempo) => {
     let cuotaInicial = 0;
     let cuotaFinal= 0;
-    let interes = 0;
-    let tasaInteresDecimal = tasaInteres/ 100;
-
+    let tasaInteresDecimal = tasaInteres / 100;
+    let periodo = tiempo;
     const cuotas = [];
     
-  
     // Transformar los años en meses
     tiempo = Math.round(tiempo * 12);
   
-    cuotaInicial = capital;
-    for (let i = 0; i < array.length; i++) {
-        cuotaFinal = cuotaInicial * Math.pow((1 + tasaInteresDecimal),tiempo);
+    cuotaInicial = monto;
+
+    cuotaFinal = cuotaInicial * Math.pow((1 + tasaInteresDecimal), tiempo);
+    for (let i = 0; i < periodo; i++) {
+        const element = array[i];
+        
         
     }
 
-    
-
-    return cuotas;
   };
